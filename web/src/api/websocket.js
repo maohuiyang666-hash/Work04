010e359478c8e1ce9f0b70e22f997defabeed0c9
mhy <3029625835@qq.com>
2026-05-24 03:55:26 +0000

    2.1

--- web/src/api/websocket.js
+++ web/src/api/websocket.js
@@ -29,14 +29,14 @@ function webSocketOnError (e) {
  */
 function webSocketOnMessage (e) {
   const data = JSON.parse(e.data)
-  const { refreshUnread, systemConfig } = data
-  if (refreshUnread) {
+  const { refreshUnread, refresh_unread, systemConfig } = data
+  if (refreshUnread || refresh_unread) {
     // 更新消息通知条数
     store.dispatch('d2admin/messagecenter/setUnread')
   }
   if (systemConfig) {
     // 更新系统配置
-    this.$store.dispatch('d2admin/settings/load')
+    store.dispatch('d2admin/settings/load')
   }
   if (data.contentType === 'SYSTEM') {
     ElementUI.Notification({
